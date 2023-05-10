1237ba5de9eb32e8ce6e65c82616ff4d8d9d3f48
Alen Vlahovljak <alen.vlahovljak@forsta.com>
2023-05-10 09:40:28 +0200

    refactor code

--- calculator.js
+++ calculator.js
@@ -2,21 +2,46 @@
 
 (function () {
   var initHandlers = function () {
-    var _numbers = [];
-    var _resultDisplayed = false;
+    var OPERATORS = /[+\-×÷^/*]/;
+    var IS_DISPLAYED = false;
 
-    function addNumHandlers() {
+    function getCurrentString(value) {
+      var currentString = value;
+      currentString = currentString.replace(/\*/g, "×");
+      currentString = currentString.replace(/\//g, "÷");
+
+      var lastChar = currentString[currentString.length - 1];
+
+      return {currentString, lastChar};
+    }
+
+    function getInputString(value) {
+      var inputString = value;
+      inputString = inputString.replace(/\*/g, "×");
+      inputString = inputString.replace(/\//g, "÷");
+
+      // forming an array of numbers. e.g. for above string it will be: numbers = ["10", "26", "33", "56", "34", "23"]
+      var numbers = inputString.split(/\+|\-|\×|\÷|\^/g);
+
+      // forming an array of operators. for above string it will be: operators = ["+", "+", "-", "*", "/"]
+      // first we replace all the numbers and dot with empty string and then split
+      var operators = inputString.replace(/[0-9]|\./g, "").split("");
+
+      return {inputString, numbers, operators};
+    }
+
+    function getNumbers(numbers) {
+      if (!(numbers instanceof NodeList)) {
+        throw new TypeError('Invalid argument: expected a NodeList');
+      }
+
       // adding click handlers to number buttons
-      for (var i = 0; i < _numbers.length; i++) {
-        _numbers[i].addEventListener("click", function (e) {
+      for (var i = 0; i < numbers.length; i++) {
+        numbers[i].addEventListener("click", function (e) {
           // storing current input string and its last character in variables - used later
-          var currentString = input.value;
-          currentString = currentString.replace(/\*/g, "×");
-          currentString = currentString.replace(/\//g, "÷");
-          var lastChar = currentString[currentString.length - 1];
+          const {currentString, lastChar} = getCurrentString(input.value);
 
-          var operators = /[+\-×÷^/*]/;
-          var substrings = (currentString + e.target.textContent).split(operators);
+          var substrings = (currentString + e.target.textContent).split(OPERATORS);
 
           for (var substr of substrings) {
             if (e.target.textContent === '.' && /\..*\./.test(substr)) {
@@ -24,19 +49,18 @@
             }
           }
 
-
           // if result is not displayed, just keep adding
-          if (_resultDisplayed === false) {
+          if (IS_DISPLAYED === false) {
             input.value += e.target.textContent;
-          } else if (_resultDisplayed === true && lastChar === "+" || lastChar === "-" || lastChar === "×" || lastChar === "÷" || lastChar === "^") {
+          } else if (IS_DISPLAYED === true && lastChar === "+" || lastChar === "-" || lastChar === "×" || lastChar === "÷" || lastChar === "^") {
             // if result is currently displayed and user pressed an operator
             // we need to keep on adding to the string for next operation
-            _resultDisplayed = false;
+            IS_DISPLAYED = false;
             input.value += e.target.textContent;
           } else {
             // if result is currently displayed and user pressed a number
             // we need clear the input string and add the new input to start the new operation
-            _resultDisplayed = false;
+            IS_DISPLAYED = false;
             input.value = "";
             input.value += e.target.textContent;
           }
@@ -45,16 +69,16 @@
       }
     }
 
-    function addOperatorHandlers() {
-      // adding click handlers to number buttons
-      for (var i = 0; i < operator.length; i++) {
-        operator[i].addEventListener("click", function (e) {
+    function getOperators(operators) {
+      if (!(operators instanceof NodeList)) {
+        throw new TypeError('Invalid argument: expected a NodeList');
+      }
 
+      // adding click handlers to number buttons
+      for (var i = 0; i < operators.length; i++) {
+        operators[i].addEventListener("click", function (e) {
           // storing current input string and its last character in variables - used later
-          var currentString = input.value;
-          currentString = currentString.replace(/\*/g, "×");
-          currentString = currentString.replace(/\//g, "÷");
-          var lastChar = currentString[currentString.length - 1];
+          const {currentString, lastChar} = getCurrentString(input.value);
 
           // if last character entered is an operator, replace it with the currently pressed one
           if (lastChar === "+" || lastChar === "-" || lastChar === "×" || lastChar === "÷" || lastChar === "^") {
@@ -71,23 +95,15 @@
       }
     }
 
-    // to do: add delay when entering with keyboard
-    // slucaj kada pokusa da unsese sa keyboard i misem da se
-    // disalbed mude dok ne unese sa jednog
-    function addCalculationHandler() {
+    function getResult(result) {
+      if (!(result instanceof HTMLElement)) {
+        throw new TypeError('Invalid argument: expected a HTMLElement');
+      }
+
       // on click of 'equal' button
       result.addEventListener("click", function () {
         // this is the string that we will be processing eg. -10+26+33-56*34/23
-        var inputString = input.value;
-        inputString = inputString.replace(/\*/g, "×");
-        inputString = inputString.replace(/\//g, "÷");
-
-        // forming an array of numbers. e.g. for above string it will be: numbers = ["10", "26", "33", "56", "34", "23"]
-        var numbers = inputString.split(/\+|\-|\×|\÷|\^/g);
-
-        // forming an array of operators. for above string it will be: operators = ["+", "+", "-", "*", "/"]
-        // first we replace all the numbers and dot with empty string and then split
-        var operators = inputString.replace(/[0-9]|\./g, "").split("");
+        const {inputString, numbers, operators} = getInputString(input.value);
 
         console.log(inputString);
         console.log(operators);
@@ -100,37 +116,31 @@
         // the final element remaining in the array will be the output
 
         var operate = {
-          _operators: [],
-          _numbers: [],
-          get(operators, numbers) {
+          _operators: [], _numbers: [], get(operators, numbers) {
             this._operators = operators;
             this._numbers = numbers;
-          },
-          exponent() {
+          }, exponent() {
             var exponent = this._operators.indexOf("^");
             while (exponent != -1) {
               this._numbers.splice(exponent, 2, this._numbers[exponent] ** this._numbers[exponent + 1]);
               this._operators.splice(exponent, 1);
               exponent = this._operators.indexOf("^");
             }
-          },
-          divide() {
+          }, divide() {
             var divide = this._operators.indexOf("÷");
             while (divide != -1) {
               this._numbers.splice(divide, 2, this._numbers[divide] / this._numbers[divide + 1]);
               this._operators.splice(divide, 1);
               divide = this._operators.indexOf("÷");
             }
-          },
-          multiply() {
+          }, multiply() {
             var multiply = this._operators.indexOf("×");
             while (multiply != -1) {
               this._numbers.splice(multiply, 2, this._numbers[multiply] * this._numbers[multiply + 1]);
               this._operators.splice(multiply, 1);
               multiply = this._operators.indexOf("×");
             }
-          },
-          add() {
+          }, add() {
             var add = this._operators.indexOf("+");
             while (add != -1) {
               // using parseFloat is necessary, otherwise it will result in string concatenation :)
@@ -138,8 +148,7 @@
               this._operators.splice(add, 1);
               add = this._operators.indexOf("+");
             }
-          },
-          subtract() {
+          }, subtract() {
             var subtract = this._operators.indexOf("-");
             while (subtract != -1) {
               this._numbers.splice(subtract, 2, this._numbers[subtract] - this._numbers[subtract + 1]);
@@ -172,67 +181,65 @@
         operate.execute('add');
 
         input.value = operate._numbers[0]; // displaying the output
-
-        _resultDisplayed = true; // turning flag if result is displayed
+        IS_DISPLAYED = true; // turning flag if result is displayed
       });
     }
 
-    function getNumbers(numbers) {
-      if (!numbers instanceof NodeList) {
-        throw new Error('Provided argument is not an array!');
+    function initKeyboard(input) {
+      if (!(input instanceof HTMLElement)) {
+        throw new TypeError('Invalid argument: expected a HTMLElement');
       }
 
-      _numbers = numbers;
+      input.addEventListener('keydown', (e) => {
+        var value = e.target.value + e.key;
+        var substrings = value.split(OPERATORS);
 
-      addNumHandlers();
-      addOperatorHandlers();
-      addCalculationHandler();
+        if (e.key.match(OPERATORS) && e.target.value.endsWith(e.key)) {
+          e.preventDefault();
+        }
+
+        for (var substr of substrings) {
+          if (e.key === '.' && /\..*\./.test(substr)) {
+            e.preventDefault();
+          }
+        }
+
+        if (["Backspace", '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '.', '+', '-', '×', '÷', '^', '/', '*'].indexOf(e.key) !== -1) {
+          return true;
+        } else {
+          e.preventDefault();
+          return false;
+        }
+      });
     }
 
     function clearInput(clear) {
-      // clearing the input on press of clear
+      if (!(clear instanceof HTMLElement)) {
+        throw new TypeError('Invalid argument: expected a HTMLElement');
+      }
+
+      // clear input when user click on clear button
       clear.addEventListener("click", function () {
         input.value = "";
       })
     }
 
     return {
-      getNumbers,
-      clearInput
+      getNumbers, getOperators, clearInput, initKeyboard, getResult
     }
   }
 
   var input = document.getElementById('input'), // input/output button
-    number = document.querySelectorAll('.numbers div'), // number buttons
-    operator = document.querySelectorAll('.operators div'), // operator buttons
+    numbers = document.querySelectorAll('.numbers div'), // number buttons
+    operators = document.querySelectorAll('.operators div'), // operator buttons
     result = document.getElementById('result'), // equal button
     clear = document.getElementById('clear'); // clear button
 
-  input.addEventListener('keydown', (e) => {
-    var value = e.target.value + e.key;
-    var operators = /[+\-×÷^/*]/;
-    var substrings = value.split(operators);
-
-    if(e.key.match(operators) && e.target.value.endsWith(e.key)){
-      e.preventDefault();
-    }
-
-    for (var substr of substrings) {
-      if (e.key === '.' && /\..*\./.test(substr)) {
-        e.preventDefault();
-      }
-    }
-
-    if (["Backspace", '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '.', '+', '-', '×', '÷', '^', '/', '*'].indexOf(e.key) !== -1) {
-      return true;
-    } else {
-      e.preventDefault();
-      return false;
-    }
-  });
-
   var init = initHandlers();
-  init.getNumbers(number);
+  init.initKeyboard(input);
+  init.getNumbers(numbers);
+  init.getOperators(operators);
+  init.getResult(result);
   init.clearInput(clear)
 })();
 
