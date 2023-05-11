839bbc54c13a366b2a216d4b473a5c3d66b25f71
Alen Vlahovljak <alen.vlahovljak@forsta.com>
2023-05-11 10:19:48 +0200

    add shift as allowed

--- calculator.js
+++ calculator.js
@@ -6,7 +6,7 @@
 
     function create() {
       var NON_NUMS = ['Delete', '+', '-', '*', '/', '×', '÷', '^'];
-      var ALLOWED_KEYS = ['Backspace', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '.'].concat(NON_NUMS);
+      var ALLOWED_KEYS = ["Shift", 'Backspace', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '.'].concat(NON_NUMS);
       var CALCULATOR_OPERATORS = ['+', '-', '×', '÷', '^'];
       var OPERATORS = /[+\-×÷^/*]/;
       var IS_DISPLAYED = false;
